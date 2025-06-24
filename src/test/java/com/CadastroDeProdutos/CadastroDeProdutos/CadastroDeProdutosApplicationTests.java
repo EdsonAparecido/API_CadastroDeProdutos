09b890f6d812efee1c5371649647ef9c1ab3e683
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-06-24 19:42:19 -0300

    Inicio do projeto

---
+++ src/test/java/com/CadastroDeProdutos/CadastroDeProdutos/CadastroDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CadastroDeProdutos.CadastroDeProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

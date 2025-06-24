cad8a11a219c40d8862e3da3dab40a75d73b6cb4
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-06-24 20:48:17 -0300

    Criação e configuração da classe Produtos Model

---
+++ src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/Model/ProdutosModel.java
@@ -0,0 +1,23 @@
+package com.CadastroDeProdutos.CadastroDeProdutos.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "tb_Produtos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProdutosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Long quantidade;
+    private Double valor;
+    private Double total;
+}

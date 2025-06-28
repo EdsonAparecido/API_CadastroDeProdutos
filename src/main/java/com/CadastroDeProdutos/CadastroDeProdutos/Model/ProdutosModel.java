69a09c55008c87f6df1aeb572244c18cb7419eb3
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-06-28 11:58:45 -0300

    Feat: Validações de entrada de dados no BD
    Adicionado annotations para validar os dados que entram no BD
    Verificando se já foi adicionado, tamanho e se está vazio.

--- src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/Model/ProdutosModel.java
+++ src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/Model/ProdutosModel.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Table(name = "tb_Produtos")
 @AllArgsConstructor
@@ -15,13 +14,18 @@ public class ProdutosModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String descricao;
-    private Long quantidade;
-    private Double valor;
-    private Double total;
 
-    public Double getTotal() {
-        return quantidade * valor;
-    }
+    @Column(unique = true, nullable = false, length = 100)
+    private String nome;
+
+    @Column(length = 300)
+    private String descricao;
+
+    @Column(nullable = false, precision = 7)
+    private Long quantidade;
+
+    @Column(nullable = false, precision = 14, scale = 2)
+    private Double valor;
+
+    private Double total;
 }

db18ddc660d629b404659151922599ba4df41772
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-07-01 22:07:15 -0300

    Feat: Configuração da Classe DTO

--- src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/DTO/ProdutosDTO.java
+++ src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/DTO/ProdutosDTO.java
@@ -1,4 +1,27 @@
 package com.CadastroDeProdutos.CadastroDeProdutos.DTO;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class ProdutosDTO {
+
+    @NotNull(message = "Nome é obrigatório.")
+    @Size(min=1, max=100, message = "Nome precisa conter de 1 a 100 caracteres.")
+    private String nome;
+
+    @Size(max = 300, message = "Tamanho máximo de 300 caracteres.")
+    private String descricao;
+
+    @NotNull(message = "Quantidade é obrigatório.")
+    @PositiveOrZero(message = "Quantidade precisa ser positivo ou zero.")
+    private BigDecimal quantidade;
+
+    @NotNull(message = "Valor é obrigatório.")
+    @PositiveOrZero(message = "Valor precisa ser positivo ou zero.")
+    private BigDecimal valor;
+
+    private BigDecimal total;
 }

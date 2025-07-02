2123cbd690c22212be2806da39c6ce1e53985884
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-07-01 22:21:33 -0300

    Feat: Criado e configurado Classe Mapper para converter DTO para Model

--- src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/Mapper/ProdutosMapper.java
+++ src/main/java/com/CadastroDeProdutos/CadastroDeProdutos/Mapper/ProdutosMapper.java
@@ -1,4 +1,18 @@
 package com.CadastroDeProdutos.CadastroDeProdutos.Mapper;
 
+import com.CadastroDeProdutos.CadastroDeProdutos.DTO.ProdutosDTO;
+import com.CadastroDeProdutos.CadastroDeProdutos.Model.ProdutosModel;
+
 public class ProdutosMapper {
+
+    public static ProdutosModel toModel(ProdutosDTO produtosDTO){
+        ProdutosModel produtosModel = new ProdutosModel();
+        produtosModel.setNome(produtosDTO.getNome());
+        produtosModel.setDescricao(produtosDTO.getDescricao());
+        produtosModel.setQuantidade(produtosDTO.getQuantidade());
+        produtosModel.setValor(produtosDTO.getValor());
+        produtosModel.setTotal(produtosDTO.getTotal());
+
+        return produtosModel;
+    }
 }
